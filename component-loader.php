b3341fdfe84b087372b659d9373d580a7a370276
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-04-04 16:17:07 +0100

    add autoloading file

---
+++ component-loader.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Detect which map provider is enabled and load the appropriate files accordingly.
+ *
+ * @package     posterno-maps
+ * @copyright   Copyright (c) 2019, Pressmodo, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       0.1.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
